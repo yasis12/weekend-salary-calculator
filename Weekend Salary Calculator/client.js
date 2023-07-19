fe588c88310f44275d0d06d48ac92d5c14bfba3a
Rafael Yasis <rafaelyasis@gmail.com>
2023-07-18 20:35:03 -0500

    set up a function and prepared the links to the DOM

--- Weekend Salary Calculator/client.js
+++ Weekend Salary Calculator/client.js
@@ -1,8 +1,25 @@
 console.log('hello Raf');
 
+// linking elements to the DOM
+let gameResults = document.querySelector('result'); 
+let restartButton = document.querySelector('restart'); 
+let gamesPlayed = document.querySelector('#numberOfGamesPlayed');
+let submitButton = document.querySelector('submit');
+
+
+
+//Defining the variables
+let firstName = '';
+let lastName ='';
+let idNumber = '';
+let jobTitle = '';
+let annualSalary = '';
+
+
+
 function salaryCalculator(event) {
     
 
 
-    
+
 }
