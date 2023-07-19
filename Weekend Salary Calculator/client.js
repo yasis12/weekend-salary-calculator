fc0dda708d7ddab6622a66eb185a453ba28f3d15
Rafael Yasis <rafaelyasis@gmail.com>
2023-07-19 14:16:55 -0500

    total mmonthly now works

--- Weekend Salary Calculator/client.js
+++ Weekend Salary Calculator/client.js
@@ -9,22 +9,27 @@ console.log('hello Raf');
 // let idNumber = 0;
 // let jobTitle = '';
 // let annualSalary = 0;
-
+let annualSalaryCombined = 0;
 
 
 function salaryCalculator(event) {
+    //linking to table
 let employeeTable = document.querySelector('#employeeTable'); 
+// linking to the form
 let firstName = document.querySelector('#firstName').value;    
 let lastName = document.querySelector('#lastName').value;
 let idNumber = document.querySelector('#idNumber').value;
 let jobTitle = document.querySelector('#jobTitle').value;
 let annualSalary = document.querySelector('#annualSalary').value;
+//linking to the total Monthly
+let totalMonthly = document.querySelector('#totalMonthly');
 
-console.log('First Name', firstName);
-console.log('Last Name', lastName);
-console.log('id Number', idNumber);
-console.log('Job title', jobTitle);
-console.log('salalry', annualSalary);
+//check to see if it works
+// console.log('First Name', firstName);
+// console.log('Last Name', lastName);
+// console.log('id Number', idNumber);
+// console.log('Job title', jobTitle);
+// console.log('salalry', annualSalary);
 
 employeeTable.innerHTML +=`
         <tr>
@@ -35,4 +40,13 @@ employeeTable.innerHTML +=`
             <th>$${annualSalary}</th>
           </tr>
 `
+annualSalaryCombined += Number(annualSalary);
+
+console.log('Combined salary thing', annualSalaryCombined);
+
+totalMonthly.innerHTML = `
+<h2>Total Monthly: $${annualSalaryCombined}</h2>
+`
+
+
 }
